6cc6832bc7f444eb2d3c7d345566e0df1cd35453
Ignat <ign.legeza@gmail.com>
2019-04-04 12:38:22 +0300

    first try

---
+++ vm_installation/install_conda.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -e
+
+# load miniconda into /tmp
+cd /tmp
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# install miniconda into common dir /opt/miniconda
+## https://docs.anaconda.com/anaconda/install/silent-mode/
+sudo mkdir /opt/miniconda && sudo chmod -R 777 /opt/miniconda
+bash Miniconda3-latest-Linux-x86_64.sh -b -f -p /opt/miniconda
+
+# add conda to PATH
+echo "export PATH=$PATH:/opt/miniconda/bin" >> /etc/profile
+export PATH="$PATH:/opt/miniconda/bin"
+
+# add new priority channel
+conda config --prepend channels conda-forge
+
+# install packages from requirements.txt
+conda install --file /tmp/git_default/vm_installation/requirements.txt --yes
+
+# make done file
+touch .conda_done
---
+++ vm_installation/install_cuda.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+set -e
+
+# common preparation
+cd /tmp
+
+# CUDA
+wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1604-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda --yes -q
+
+# cuDNN, get link on https://developer.nvidia.com/cudnn
+wget -O cudnn-10.1-linux-x64.tgz $1
+tar -xzvf cudnn-10.1-linux-x64.tgz
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h
+
+# install pytorch
+conda install pytorch torchvision cudatoolkit=10.0 -c pytorch --yes -q
+
+# make done file
+touch .cuda_done
