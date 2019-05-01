136581a547095d3e9adc51dce09e6e3c2cde21c0
Ignat <ign.legeza@gmail.com>
2019-05-01 16:32:03 +0300

    Change PATH modification line

--- vm_installation/install_conda.sh
+++ vm_installation/install_conda.sh
@@ -6,12 +6,12 @@ cd /tmp
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # install miniconda into common dir /opt/miniconda
-## https://docs.anaconda.com/anaconda/install/silent-mode/
+## guide: https://docs.anaconda.com/anaconda/install/silent-mode/
 sudo mkdir /opt/miniconda && sudo chmod -R 777 /opt/miniconda
 bash Miniconda3-latest-Linux-x86_64.sh -b -f -p /opt/miniconda
 
 # add conda to PATH
-echo "export PATH=$PATH:/opt/miniconda/bin" >> /etc/profile
+sudo echo "export PATH=$PATH:/opt/miniconda/bin" >> /etc/environment # /etc/profile
 export PATH="$PATH:/opt/miniconda/bin"
 
 # add new priority channel
